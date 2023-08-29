ad50ed63bae2409a901bbc14fd0b8e1a754cd0e2
DaniSodeiro <danielasodeirosilva@gmail.com>
2023-08-28 22:01:03 -0300

    "refactor : criação de classe no código

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 // Importa o módulo readline-sync para interagir com o usuário via linha de comando
 import readline from "readline-sync";
 
-// Cria um array chamado propriedades para armazenar as propriedades de CSS
-const propriedades = [];
+class PropriedadesCSS {
+  constructor() {
+    this.propriedades = [];
+  }
 
-// Função propriedadesPrompt que irá coletar as propriedades de CSS
-function propriedadesPrompt() {
-    // Pede ao usuário que digite uma propriedade de CSS ou "SAIR" para encerrar
-    let entrada = readline.question("Digite uma propriedade de CSS (ou 'SAIR' para encerrar): ").toUpperCase();
+  coletarPropriedades() {
+    let entrada = readline.question(
+      "Digite uma propriedade de CSS (ou 'SAIR' para encerrar): "
+    ).toUpperCase();
 
     // Verifica se o usuário digitou "SAIR"
     if (entrada === "SAIR") {
-        // Se sim, ordena e exibe as propriedades digitadas
-        propriedades.sort().forEach((propriedade) => {
-            console.log(propriedade);
-        });
+      this.exibirPropriedadesOrdenadas();
     } else {
-        // Se não, adiciona a propriedade ao array propriedades e chama a função novamente
-        propriedades.push(entrada);
-        propriedadesPrompt();
+      this.propriedades.push(entrada);
+      this.coletarPropriedades();
     }
+  }
+
+  exibirPropriedadesOrdenadas() {
+    this.propriedades.sort().forEach((propriedade) => {
+      console.log(propriedade);
+    });
+  }
 }
 
-// Chama a função propriedadesPrompt para começar a coletar as propriedades
-propriedadesPrompt();
-
+const propriedadesCSS = new PropriedadesCSS();
+propriedadesCSS.coletarPropriedades();
