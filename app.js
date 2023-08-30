c5a1bbbc8165958addcd40de8a6adabf3b2763ea
DaniSodeiro <danielasodeirosilva@gmail.com>
2023-08-30 11:46:20 -0300

    Adicionada validação para evitar entrada em branco

--- app.js
+++ app.js
@@ -2,29 +2,32 @@
 import readline from "readline-sync";
 
 class PropriedadesCSS {
-  constructor() {
-    this.propriedades = [];
-  }
-
-  coletarPropriedades() {
-    let entrada = readline.question(
-      "Digite uma propriedade de CSS (ou 'SAIR' para encerrar): "
-    ).toUpperCase();
-
-    // Verifica se o usuário digitou "SAIR"
-    if (entrada === "SAIR") {
-      this.exibirPropriedadesOrdenadas();
-    } else {
-      this.propriedades.push(entrada);
-      this.coletarPropriedades();
+    constructor() {
+        this.propriedades = [];
     }
-  }
 
-  exibirPropriedadesOrdenadas() {
-    this.propriedades.sort().forEach((propriedade) => {
-      console.log(propriedade);
-    });
-  }
+    coletarPropriedades() {
+        let entrada = readline.question(
+            "Digite uma propriedade de CSS (ou 'SAIR' para encerrar): "
+        ).toUpperCase();
+
+        // Verifica se o usuário digitou algo que não seja vazio
+        if (entrada.trim() === "") {
+            console.log("Por favor, digite uma propriedade válida.");
+            this.coletarPropriedades(); // Chama a função novamente para obter uma entrada válida
+        } else if (entrada === "SAIR") {
+            this.exibirPropriedadesOrdenadas();
+        } else {
+            this.propriedades.push(entrada);
+            this.coletarPropriedades();
+        }
+    }
+
+    exibirPropriedadesOrdenadas() {
+        this.propriedades.sort().forEach((propriedade) => {
+            console.log(propriedade);
+        });
+    }
 }
 
 const propriedadesCSS = new PropriedadesCSS();
